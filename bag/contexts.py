580a0d5e11a3a6570acd44b2c23f9b4f47bb2381
SantiagoYanezFerreiro <santiagoyanez@mailbox.org>
2021-09-15 21:55:00 +0000

    shopping bag app

---
+++ bag/contexts.py
@@ -0,0 +1,28 @@
+from decimal import DEcimal
+
+def bag_contents(request):
+
+    bag_items = []
+    total = 0
+    product_count = 0
+
+    if total < settings.FREE_DELIVERY_TRESHOULD
+        delivery = total * Decimal (settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        free_delivery_delta = settings.FREE_DELIVERY_TRESHOULD  - total
+    else:
+        delivery = 0
+        free_delivery_delta = 0
+
+    grand_total = delivery + total
+
+    context = {
+        'bag_items': bag_items,
+        'total': total,
+        'product_count': product_count,
+        'delivery': delivery,
+        'free_delivery_delta': free_delivery_delta,
+        'free_delviery_treshold': settings.FREE_DELIVERY_TRESHOULD,
+        'grand_total': grand_total,
+    }
+
+    return context
