f64978e2be0d4fb9a3537d96348cefdeee610a59
SantiagoYanezFerreiro <santiagoyanez@mailbox.org>
2021-09-26 15:03:19 +0000

    added aboutus app

--- top_notch_coffee/urls.py
+++ top_notch_coffee/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('contact/', include('contact.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
+    path('aboutus/', include('aboutus.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
