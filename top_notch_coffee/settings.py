e5eead226abea15bdc8adfbf2871739386868874
SantiagoYanezFerreiro <santiagoyanez@mailbox.org>
2021-10-02 12:05:32 +0000

    fixed mismatch named variable

--- top_notch_coffee/settings.py
+++ top_notch_coffee/settings.py
@@ -181,7 +181,7 @@ if 'USE_AWS' in os.environ:
     }
 
 if 'USE_AWS' in os.environ:
-    ASW_STORAGE_BUCKET_NAME = 'topnotchoffeeawsbucket'
+    AWS_STORAGE_BUCKET_NAME = 'topnotchoffeeawsbucket'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY_ID = os.environ.get('AWS_SECRET_ACCESS_KEY')
