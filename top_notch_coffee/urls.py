546fe4dd817a2706040103fd24dc08d47ae26b22
SantiagoYanezFerreiro <santiagoyanez@mailbox.org>
2021-10-09 13:46:57 +0000

    added 404 and 500 errors templates

--- top_notch_coffee/urls.py
+++ top_notch_coffee/urls.py
@@ -16,6 +16,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path
+from . import views
+from django.conf.urls import handler404, handler500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +31,6 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('aboutus/', include('aboutus.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'top_notch_coffee.views.error_404'
+handler500 = 'top_notch_coffee.views.error_500'
